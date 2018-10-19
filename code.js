802d6b17d2f709e1f3de6f1223601b088c332db5
Evan Garrett <evan@fitfox.de>
2018-10-19 12:13:11 +0200

    Automatic redirect

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 $.getJSON('http://localhost/api/studio')
 	.then(response => {
 		const id = response._embedded.items[0].products[0].id;
-		$('.checkout-link').html(`<a href="http://localhost:3000/studios/checkout/${id}">Here is the link to Checkout Product - Latest ID</a>`);
+		window.location.replace(`http://localhost:3000/studios/checkout/${id}`);
 
 	});
