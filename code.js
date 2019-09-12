0cc43cd75380fe812458661c86690f99a4f4fbcf
Evan Garrett <evan@fitfox.de>
2019-09-12 13:34:45 +0200

    Update for new checkout url

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
+const utilities = {
+	getCheckoutPathWithId: (id) =>`/checkout/?products[0][id]=${id}&products[0][quantity]=1`,
+}
+
 $.getJSON('http://localhost/api/studio')
 	.then(response => {
 		const id = response._embedded.items[0].products[0].id;
@@ -5,7 +9,7 @@ $.getJSON('http://localhost/api/studio')
 		// window.location.replace(`http://localhost:3000/studios/checkout/${id}`);
 
 		$('.studio-checkout-link').html(`
-		<a href=http://localhost:3000/checkout/${id}>
+		<a href=http://localhost:3000${utilities.getCheckoutPathWithId(id)}>
 			<button>Studio Product Checkout </button>
 		</a>
 		`)
@@ -24,7 +28,7 @@ $.getJSON('http://localhost/api/event')
 	.then(response => {
 		const id = response.products[0].id
 		$('.event-checkout-link').html(`
-		<a href=http://localhost:3000/checkout/${id}>
+		<a href=http://localhost:3000${utilities.getCheckoutPathWithId(id)}>
 			<button>Free Event Product Checkout</button>
 		</a>
 		`)
@@ -41,7 +45,7 @@ $.getJSON('http://localhost/api/event')
 	.then(response => {
 		const id = response.products[0].id
 		$('.paid-event-checkout-link').html(`
-		<a href=http://localhost:3000/checkout/${id}>
+		<a href=http://localhost:3000${utilities.getCheckoutPathWithId(id)}>
 			<button>Paid Event Product Checkout</button>
 		</a>
 		`)
